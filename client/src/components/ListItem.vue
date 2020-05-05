9b5d2dc3c4689ac15f7e7f0c7af99fd4f671f794
Jack Quigley <jackaquigley@gmail.com>
2020-05-05 16:22:39 +0100

    added list + list item

--- client/src/components/ListItem.vue
+++ client/src/components/ListItem.vue
@@ -1,12 +1,19 @@
 <template lang="html">
-
+<div class="ListItem">
+  <p> Customer Name: {{booking.name}} Customer Email: {{booking.email}}  Customer Checked In:{{booking.check_in}} </p>
+</div>
 </template>
 
 <script>
+
 export default {
   name: 'list-item',
+  props: ['booking']
 }
 </script>
 
 <style lang="css" scoped>
+.ListItem {
+  border: 0.5px black solid;
+}
 </style>
